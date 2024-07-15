359453187c33a77881dd2d3f3f0809d4b985c94b
arafehr <arrahat@mun.ca>
2024-07-15 14:29:20 +0500

    Make website responsive for tablet

--- src/components/NextVideo/NextVideo.jsx
+++ src/components/NextVideo/NextVideo.jsx
@@ -15,17 +15,6 @@ const NextVideo = ({ video }) => {
           </div>
         </div>
       </div>
-      <div className="next__video">
-        <div className="next__video__card">
-          <div className="card__media">
-            <img src={video.image} alt="video-img" />
-          </div>
-          <div className="video__info">
-            <div className="video__title">{video.title}</div>
-            <div className="video__author">{video.channel}</div>
-          </div>
-        </div>
-      </div>
     </>
   )
 }
